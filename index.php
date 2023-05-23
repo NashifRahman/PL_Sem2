9bcf8344b3e97e437ecaac185da231116235a732
NashifRahman <nashifrahman2004@gmail.com>
2023-05-23 09:50:22 +0700

    kodingan PL semester 2

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-function check_link ($link) {
-    if (isset($_GET["module"])){
+function check_link($link)
+{
+    if (isset($_GET["module"])) {
         $nama_file = $_GET["module"];
 
         if ($link == $nama_file) {
@@ -26,29 +27,41 @@ function check_link ($link) {
     <div id="header1">
         <div id="container">
             <div id="header">
-                </div>
-                <div id="bungkus">
+            </div>
+            <div id="bungkus">
+                <div id="bungkus2">
                     <div id="sidebar">
-                    <h3>navigasi</h3>
-                    <ul id="navmenu">
-                        <li><a href="index.php" class="<?= check_link("")?>">profil</a></li>
-                        <li><a href="?module=lihat#pos" class="<?= check_link("lihat")?>">galeri</a></li>
-                        <li><a href="?module=cari#pos" class="<?= check_link("cari")?>">jadwal kuliah</a></li>
-                    </ul>
+                        <h3>navigasi</h3>
+                        <ul id="navmenu">
+                            <li><a href="index.php" class="<?= check_link("") ?>">Form</a></li>
+                            <li><a href="?module=lihat#pos" class="<?= check_link("lihat") ?>">Lihat</a></li>
+                            <li><a href="?module=cari#pos" class="<?= check_link("cari") ?>">Cari</a></li>
+                        </ul>
+                    </div>
+                    <div id="in">
+                        <form action="?module=loginproc#pos" method="post" class="form">
+                            Username
+                            <input type="text" name="username" style="width:150px">
+
+                            Password
+                            <input type="password" name="pass" style="width:150px">
+                            <input type="submit" value="login">
+                        </form>
+                    </div>
                 </div>
                 <div id="page">
-                    <?php 
-                    if(isset($_GET["module"])){
+                    <?php
+                    if (isset($_GET["module"])) {
                         $Nama_file = $_GET["module"];
                         include "module/$Nama_file.php";
-                    }else
-                    include "uinform.php";
+                    } else
+                        include "uinform.php";
                     ?>
                 </div>
             </div>
-            
+
             <div id="clear"></div>
-            
+
             <div id="footer">
                 <p>&copy; 2010</p>
             </div>
--- module/cari.php
+++ module/cari.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="scale=1.0">
     <title>Document</title>
+    <style type="text/css">
+        td {
+            font-size: 12px;
+        }
+    </style>
 </head>
+
 <body>
     <h2>Cari Member</h2>
     <form action="?module=cari" method="get">
@@ -17,51 +24,72 @@
     <h2>Hasil pencarian</h2>
     <?php
     include "koneksi.php";
-    if(isset($_GET['username'])){
-    $username = $_GET['username'];
-    $hasil = mysqli_query($conn, "SELECT * FROM register where `username` like '$username'");
+    if (isset($_GET['username'])) {
+        $username = $_GET['username'];
+        $hasil = mysqli_query($conn, "SELECT * FROM register where `username` like '$username'");
     }
     ?>
     <div>
-    <table  border="1">
-        <tr>
-            <td >id</td>
-            <td >nama depan</td>
-            <td >nama belakang</td>
-            <td >username</td>
-            <td >password</td>
-            <td >usia</td>
-            <td >jk</td>
-            <td >ttl</td>
-            <td >email</td>
-            <td >notel</td>
-            <td colspan="2">aksi</td>
-        </tr>
-    <?php
-    if(isset($_GET['username'])):
-    while($buff = mysqli_fetch_assoc($hasil)):
-        ?>
-        <tr>
-            <td ><?php echo $buff["id"];?></td>
-            <td ><?php echo $buff['namadep'];?></td>
-            <td ><?php echo $buff["namabel"];?></td>
-            <td ><?php echo $buff["username"];?></td>
-            <td ><?php echo $buff["password"];?></td>
-            <td ><?php echo $buff["usia"];?></td>
-            <td ><?php echo $buff["jk"];?></td>
-            <td ><?php echo $buff["ttl"];?></td>
-            <td ><?php echo $buff["email"];?></td>
-            <td ><?php echo $buff["notel"];?></td>
-            <td ><a href="edit.php?id=<?php echo $buff["id"];?>">edit</a></td>
-            <td ><a href="hapus.php?id=<?php echo $buff["id"];?>">hapus</a></td>
-        </tr>
-    <?php
-    endwhile;
-endif;
-?>
-    </table>
+        <table border="1">
+            <tr>
+                <td>id</td>
+                <td>nama depan</td>
+                <td>nama belakang</td>
+                <td>username</td>
+                <td>password</td>
+                <td>usia</td>
+                <td>jk</td>
+                <td>ttl</td>
+                <td>email</td>
+                <td>notel</td>
+                <td colspan="2">aksi</td>
+            </tr>
+            <?php
+            if (isset($_GET['username'])):
+                while ($buff = mysqli_fetch_assoc($hasil)):
+                    ?>
+                    <tr>
+                        <td>
+                            <?php echo $buff["id"]; ?>
+                        </td>
+                        <td>
+                            <?php echo $buff['namadep']; ?>
+                        </td>
+                        <td>
+                            <?php echo $buff["namabel"]; ?>
+                        </td>
+                        <td>
+                            <?php echo $buff["username"]; ?>
+                        </td>
+                        <td>
+                            <?php echo $buff["password"]; ?>
+                        </td>
+                        <td>
+                            <?php echo $buff["usia"]; ?>
+                        </td>
+                        <td>
+                            <?php echo $buff["jk"]; ?>
+                        </td>
+                        <td>
+                            <?php echo $buff["ttl"]; ?>
+                        </td>
+                        <td>
+                            <?php echo $buff["email"]; ?>
+                        </td>
+                        <td>
+                            <?php echo $buff["notel"]; ?>
+                        </td>
+                        <td><a href="edit.php?id=<?php echo $buff["id"]; ?>">edit</a></td>
+                        <td><a href="hapus.php?id=<?php echo $buff["id"]; ?>">hapus</a></td>
+                    </tr>
+                    <?php
+                endwhile;
+            endif;
+            ?>
+        </table>
     </div>
     <br>
     <a href="index.php"><---- Kembali ke index</a>
 </body>
+
 </html>
